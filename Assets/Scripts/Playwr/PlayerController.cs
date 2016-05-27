2bb37df1445cbf6dc1656900d8b259abec4e27af
PiotrusWatson <piotruswatson@gmail.com>
2016-05-27 19:10:33 +0100

    Made player animate walk cycle when moving, and set up basic state machine for anims at hand.
    Looks bad.
    
    Need to fix:
    Speed at which animation plays
    Rigid feeling of controls
    The players tendency to bounce up and down like an overexcitable prick

--- Assets/Scripts/Playwr/PlayerController.cs
+++ Assets/Scripts/Playwr/PlayerController.cs
@@ -2,9 +2,19 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+
+	/*Todo: make movement not feel terrible - add some resistance to axis maybe?
+	 * Rotate player according to axis pressed - basically specific rotation values depending on whether horizontal or vertical > or < 0
+	 * Refactor everything and make code use functions nicely 
+	 * Do sword
+	 * maybe look into fancy animation stuff like blend trees etc - for now can make do with bools and specific rotation code but w/e
+	 */
 	Rigidbody2D rb2D;
 	float horizontal;
 	float vertical;
+
+
+	Animator animator;
 
 	bool isMoving; //This bool will be used for animating the walk cycle :)
 
@@ -13,6 +23,7 @@ public class PlayerController : MonoBehaviour {
 	void Awake () 
 	{
 		rb2D = GetComponent<Rigidbody2D> (); //boilerplate setting up
+		animator = GetComponent<Animator>();
 
 	}
 	
@@ -23,6 +34,11 @@ public class PlayerController : MonoBehaviour {
 
 		if (horizontal != 0) //This code ensures the player doesnt move diagonally
 			vertical = 0;
+
+		isMoving = horizontal != 0 || vertical != 0; //if player is moving, it = true, else it = false
+
+		animator.SetBool ("IsMoving", isMoving);
+
 
 		rb2D.velocity = new Vector2 (horizontal, vertical) * speed; //moves the player 
 	}
