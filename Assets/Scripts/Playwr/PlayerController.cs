167fdab4a2bec464553593350ca109004caea4a7
PiotrusWatson <piotruswatson@gmail.com>
2016-05-27 18:42:33 +0100

    Alright so I did quite a lot in this bit
    Made first pass at player movement it's basic and feels like shit
    
    Converted the ASE files to .png and added an animation controller. Also added the animations in general to the player gameobject.
    
    Made a really hilariously bad looking UI line between the split of the screen

---
+++ Assets/Scripts/Playwr/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	Rigidbody2D rb2D;
+	float horizontal;
+	float vertical;
+
+	bool isMoving; //This bool will be used for animating the walk cycle :)
+
+	public int speed = 10;
+	// Use this for initialization
+	void Awake () 
+	{
+		rb2D = GetComponent<Rigidbody2D> (); //boilerplate setting up
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontal = Input.GetAxis ("Horizontal"); //storing variables so i can do checks against them :)
+		vertical =  Input.GetAxis ("Vertical"); 
+
+		if (horizontal != 0) //This code ensures the player doesnt move diagonally
+			vertical = 0;
+
+		rb2D.velocity = new Vector2 (horizontal, vertical) * speed; //moves the player 
+	}
+}
